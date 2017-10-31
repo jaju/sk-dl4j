740d7a18e50f84aea46d05c00579bbbc340af59c
Ravindra R. Jaju <jaju@msync.org>
2017-10-31 08:23:47 +0530

    Added a comment on use of the returned trained-model.

--- src/sk_dl4j/core.clj
+++ src/sk_dl4j/core.clj
@@ -3,5 +3,7 @@
   (:import (org.nd4j.linalg.activations Activation)))
 
 (comment
+  ;; Run me! REPL-up, and evaluate the following form. Nothing else to be configured.
+  ;; It returns the trained model too. You can capture that in a var to play with in the REPL.
   (xor-nn/run :hl-activation-fn Activation/HARDTANH
               :ol-activation-fn Activation/SOFTMAX))
