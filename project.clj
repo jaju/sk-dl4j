150f9e18847b92bb451ae9e0ad75564bd0403829
Ravindra R. Jaju <jaju@msync.org>
2017-10-31 11:26:11 +0530

    The simple joys of being on the edge. Updated clojure to 1.9.0-beta3, for no useful reason.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://starter-kit.msync.org/deeplearning4j"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.deeplearning4j/deeplearning4j-core "0.9.1"]
                  [org.nd4j/nd4j-native-platform "0.9.1"]
